cfb5c6f611a357aec5359e60fba4e0e2d586ae4e
mi-fasol <minyoong39@naver.com>
2023-04-07 00:55:57 +0900

    fix: Post 데이터 Person 자료형 Long으로 수정

--- src/main/java/com/example/haemo/haemo/Data/Post.java
+++ src/main/java/com/example/haemo/haemo/Data/Post.java
@@ -28,49 +28,8 @@ public class Post {
     private String nickname;
 
     @Column(name = "person")
-    private String person;
+    private Long person;
 
     @Column(name = "category")
     private String category;
-
-    public Long getpId(){
-        return pId;
-    }
-
-    public void setPId(){
-        this.pId = pId;
-    }
-
-    public String getTitle(){
-        return title;
-    }
-
-    public void setTitle(){
-        this.title = title;
-    }
-    public String getContent(){
-        return content;
-    }
-
-    public void setContent(){
-        this.content = content;
-    }
-    public String getNickname(){
-        return nickname;
-    }
-    public void setNickname(){
-        this.nickname= nickname;
-    }
-    public String getpPerson(){
-        return person;
-    }
-    public void setPerson(){
-        this.person = person;
-    }
-    public String getpCategory(){
-        return category;
-    }
-    public void setCategory(){
-        this.category = category;
-    }
 }
