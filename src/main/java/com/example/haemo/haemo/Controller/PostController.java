2bbeca38e8d54f74212407b74e254ad751999f1d
mi-fasol <minyoong39@naver.com>
2023-04-12 23:03:59 +0900

    feat: 프론트에 게시물 보내주는 기능 추가

--- src/main/java/com/example/haemo/haemo/Controller/PostController.java
+++ src/main/java/com/example/haemo/haemo/Controller/PostController.java
@@ -1,12 +1,15 @@
 package com.example.haemo.haemo.Controller;
 
 import com.example.haemo.haemo.Data.Post;
+import com.example.haemo.haemo.Repository.PostRepository;
 import com.example.haemo.haemo.Service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -16,9 +19,18 @@ public class PostController {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @PostMapping(produces = "application/json")
     public ResponseEntity<Post> savePost(@RequestBody Post post) {
         Post savedPost = postService.savePost(post);
         return new ResponseEntity<>(savedPost, HttpStatus.CREATED);
+    }
+
+    @GetMapping(produces = "application/json")
+    @ResponseBody
+    public List<Post> fetchPost(){
+        return postRepository.findAll();
     }
 }
