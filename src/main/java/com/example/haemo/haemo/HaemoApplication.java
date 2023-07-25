ccfa912bb54fe92864b3240fdcf132228b6a9251
mi-fasol <minyoong39@naver.com>
2023-07-26 00:06:56 +0900

    feat: propertySource 추가

--- src/main/java/com/example/haemo/haemo/HaemoApplication.java
+++ src/main/java/com/example/haemo/haemo/HaemoApplication.java
@@ -3,8 +3,10 @@ package com.example.haemo.haemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@PropertySource("classpath:/url.properties")
 public class HaemoApplication {
 
 	public static void main(String[] args) {
