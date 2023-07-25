e312661284cae9e1d371cb97b59b7668f0b57518
mi-fasol <minyoong39@naver.com>
2023-07-25 23:46:05 +0900

    fix: testFile 오류 삭제

--- src/test/java/com/example/haemo/haemo/HaemoApplicationTests.java
+++ src/test/java/com/example/haemo/haemo/HaemoApplicationTests.java
@@ -1,12 +1,8 @@
 package com.example.haemo.haemo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class HaemoApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
