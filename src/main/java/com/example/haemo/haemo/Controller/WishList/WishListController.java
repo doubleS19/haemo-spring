4875b04d9ad0fd0e1e899dabd052c0530610cc22
mi-fasol <minyoong39@naver.com>
2023-08-10 20:44:13 +0900

    fix: 찜 추가 시 1 안 올라가던 오류 수정

--- src/main/java/com/example/haemo/haemo/Controller/WishList/WishListController.java
+++ src/main/java/com/example/haemo/haemo/Controller/WishList/WishListController.java
@@ -4,6 +4,7 @@ import com.example.haemo.haemo.Controller.HotPlace.HotPlaceController;
 import com.example.haemo.haemo.Data.HotPlace.HotPlace;
 import com.example.haemo.haemo.Data.Post.Post;
 import com.example.haemo.haemo.Data.WishList.WishList;
+import com.example.haemo.haemo.Repository.HotPlace.HotPlaceRepository;
 import com.example.haemo.haemo.Repository.WishList.WishListRepository;
 import com.example.haemo.haemo.Service.HotPlace.HotPlaceService;
 import com.example.haemo.haemo.Service.WishList.WishListService;
@@ -27,6 +28,8 @@ public class WishListController {
     @Autowired
     private HotPlaceService hotPlaceService;
 
+    @Autowired HotPlaceRepository hotPlaceRepository;
+
     @GetMapping(produces = "application/json")
     @ResponseBody
     public List<WishList> getAllWishList() {
@@ -38,6 +41,7 @@ public class WishListController {
         WishList savedWish = wishListService.addWish(wishList);
         HotPlace wishedPlace = hotPlaceService.getHotPlaceById(savedWish.getPId());
         wishedPlace.setWishing(wishedPlace.getWishing()+1);
+        hotPlaceRepository.save(wishedPlace);
         return new ResponseEntity<>(savedWish, HttpStatus.CREATED);
     }
 
