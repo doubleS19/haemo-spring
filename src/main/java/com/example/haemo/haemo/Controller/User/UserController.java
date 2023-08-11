2abd1b001ba59404346ec57ddd6d6ffe959cbed3
mi-fasol <minyoong39@naver.com>
2023-08-11 21:01:57 +0900

    feat: 모든 user 반환 및 id로 유저 정보 찾는 api 추가

--- src/main/java/com/example/haemo/haemo/Controller/User/UserController.java
+++ src/main/java/com/example/haemo/haemo/Controller/User/UserController.java
@@ -1,5 +1,6 @@
 package com.example.haemo.haemo.Controller.User;
 
+import com.example.haemo.haemo.Data.HotPlace.HotPlace;
 import com.example.haemo.haemo.Data.User.User;
 import com.example.haemo.haemo.Repository.User.UserRepository;
 import com.example.haemo.haemo.Service.User.UserService;
@@ -7,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -29,5 +33,19 @@ public class UserController {
     public User getUserByNickname(@PathVariable String nickname) {
         User findedUser = userService.findUserByNickname(nickname);
         return findedUser;
+    }
+
+    @GetMapping(produces = "application/json")
+    @ResponseBody
+    public List<User> getAllUser() {
+        return userRepository.findAll();
+    }
+
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity<User> getHotPlaceById(@PathVariable Long id) {
+        Optional<User> postOptional = userRepository.findById(id);
+        return postOptional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
