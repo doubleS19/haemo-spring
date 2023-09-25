0b2251558cbcdef8d61a36458ff1f42cf1153417
mi-fasol <minyoong39@naver.com>
2023-09-25 19:53:49 +0900

    feat: 로그인 관련 로직 추가 (수정 및 구조 변경 필요)

--- src/main/java/com/example/haemo/haemo/Data/User/Login.java
+++ src/main/java/com/example/haemo/haemo/Data/User/Login.java
@@ -7,16 +7,12 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "user")
+@Table(name = "login")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Login {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "lId")
-    private Long lId;
 
     @Column(name = "studentId")
     private String studentId;
